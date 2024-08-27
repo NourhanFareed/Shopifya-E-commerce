0774139245242cf008c9bbf913e0c623c4b04d23
NourhanFareed <nourfareed433@gmail.com>
2024-08-27 20:31:31 +0300

    the Least

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -76,7 +76,7 @@ export default function Cart() {
                                     <p className="text-sm text-gray-900 dark:text-white font-bold">including VAT</p>
                                 </div>
                             </div>
-                            <Link to={"/shippingAddress/" + cartProducts?._id}><button className="mt-6 w-full rounded-md bg-blue-500 dark:bg-[#003783] dark:hover:bg-[#0061a5] py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button></Link>
+                            <Link to={"/shippingAddress/" + cartProducts?.data._id}><button className="mt-6 w-full rounded-md bg-blue-500 dark:bg-[#003783] dark:hover:bg-[#0061a5] py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button></Link>
                         </div>
                     </div>
                     <ScrollingUpIcon />
